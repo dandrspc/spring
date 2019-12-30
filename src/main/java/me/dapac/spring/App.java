17efcea60332eabf8a44317edc8b2be59c87226e
Daniel Palacios Carabalí <dapalca@gmail.com>
2019-12-30 10:38:24 -0500

    bean scopes: singleton & prototype

--- src/main/java/me/dapac/spring/App.java
+++ src/main/java/me/dapac/spring/App.java
@@ -10,7 +10,12 @@ public class App {
 //        XML configuraction
         ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
         Person person = (Person) appContext.getBean("person");
+        person.setId(1);
+        person.setName("Daniel");
+        person.setNickname("dandrspc");
+        Person person2 = (Person) appContext.getBean("person");
 
         System.out.println(person);
+        System.out.println(person2);
     }
 }
